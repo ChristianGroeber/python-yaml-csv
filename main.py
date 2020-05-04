46add1888a93173d8ac33e950682a94ab0f7625f
ChristianGroeber <christian.groeber@bluewin.ch>
2020-05-04 11:38:28 +0200

    added encoding option

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import csv
 import os
 
 
-def csv_to_yaml(input_csv, output_yaml):
-    f = open(input_csv, 'r')
+def csv_to_yaml(input_csv, output_yaml, encoding='utf-8'):
+    f = open(input_csv, 'r', encoding=encoding)
     out = open(output_yaml, 'w+')
     for line in f.readlines():
         if not line.startswith(';'):
             print(line)
-            out.write(line.replace(';', ':'))
+            out.write(line.replace(';', ': '))
